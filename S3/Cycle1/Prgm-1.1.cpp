2bb607ee0f8b09d8913202c824fd74dc84692d11
Unintended <devreader795@gmail.com>
2021-01-10 14:19:34 +0530

    prgm1 correct & ques update

--- S3/Cycle1/Prgm-1.1.cpp
+++ S3/Cycle1/Prgm-1.1.cpp
@@ -1,16 +1,35 @@
 
-// Program-1: 
+// Program-1:
 
+    /* A phone number, such as (212) 767-8900, can be thought of as having three
+        parts: the area code (212), the exchange (767), and the number (8900).
+        Write a C++ program that uses a structure to store these three parts of a
+        phone number separately. Call the structure phone. Create two structure
+        variables of type phone. Initialize one, and have the user input a number
+        for the other one. Then display both numbers. The interchange might look
+        like this:
+	    Enter your area code, exchange, and number: 415 555 1212
+	        My number is (212) 767-8900
+	        Your number is (415) 555-1212 
+    */
 
 #include <iostream>
-#include <ctime>
-#include <vector>
 using namespace std;
+
+struct phone
+{
+    int ar_code, exchange, num;
+} p, p1;
 
 int main()
 {
-    cout << "Hello World";
+    p1.ar_code = 212, p1.exchange = 767, p1.num = 8900;
+    cout << "Enter your area code, exchange and number:  ";
+    cin >> p.ar_code >> p.exchange >> p.num;
+    cout << "\nMy number is: (" << p1.ar_code << ") " << p1.exchange << "-" << p1.num;
+    cout << "\nYour number is: (" << p.ar_code << ") " << p.exchange << "-" << p.num << endl;
 
+    cin.ignore();
     cin.get();
     return 0;
 }
